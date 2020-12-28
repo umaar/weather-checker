a332901638a588e92b04145e895f195d6636f9b7
Umar Hansa <umar.hansa@gmail.com>
2020-12-28 14:38:57 +0000

    ios fixes

--- src/server/routes/resolve-location.ts
+++ src/server/routes/resolve-location.ts
@@ -8,10 +8,12 @@ import {
 import getBaseURL from '../lib/get-base-url.js';
 
 async function resolveLocation(request: express.Request, response: express.Response) {
-	const {
-		query,
+	let {
+		query = '',
 		'query-type': queryType
 	} = request.query;
+
+	query = String(query).trim();
 
 	if (!query || !queryType) {
 		throw new Error('Noooo no valid form data');
@@ -20,10 +22,10 @@ async function resolveLocation(request: express.Request, response: express.Respo
 	let results = [];
 
 	if (queryType === 'coordinates') {
-		results = await getLocationFromLatLon(String(query));
+		results = await getLocationFromLatLon(query);
 	} else {
 		// Regular search string like 'brighton'
-		results = await searchForLocation(String(query));
+		results = await searchForLocation(query);
 	}
 
 	results = results.map(result => {
