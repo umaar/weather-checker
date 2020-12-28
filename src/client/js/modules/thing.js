dc9209826f8bbd2e6bb6c1346e24a7a965c17bad
Umar Hansa <umar.hansa@gmail.com>
2020-12-28 15:42:30 +0000

    update

--- src/client/js/modules/thing.js
+++ src/client/js/modules/thing.js
@@ -1,3 +1,33 @@
-export default function hello() {
-	console.log('hello');
+function getCurrentLocation() {
+	return new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(({coords}) => {
+			resolve(coords);
+		}, reject);
+	});
 }
+
+function hello() {
+	const form = document.querySelector('form');
+	if (!form) return;
+
+	const btn = document.createElement('button');
+	btn.textContent = 'Use current location';
+	form.appendChild(btn);
+
+	btn.addEventListener('click', async evt => {
+		evt.preventDefault();
+		btn.disabled = true;
+		btn.textContent = 'Please wait';
+		document.querySelector('#coordinates').click();
+		try {
+			const {latitude, longitude} = await getCurrentLocation();
+			document.querySelector('#query').value = `${latitude},${longitude}`;
+			form.querySelector('[type="submit"]').click();
+		} catch {
+			btn.disabled = false;
+			btn.textContent = 'Use current location';
+		}
+	});
+}
+
+export default hello;
